723df07485167a2b3ac46df58470db5e25fee0cc
Josephine <maryjohofmeister@gmail.com>
2015-06-29 08:10:25 -0300

    Confidence Intervals for GLMMS Example Code

---
+++ ShinyTutorial.R
@@ -0,0 +1,9 @@
+install.packages("shiny")
+#http://shiny.rstudio.com/tutorial/lesson1/
+library(shiny)
+runExample("01_hello")
+#Set working drive - for work
+setwd("C:/Users/carvalhom/Documents/R")#set data library
+
+runApp("my_app")
+runExample("02_text") # tables and data frames
