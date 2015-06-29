ae41720c97938cd075d8cef517ada728913d4b45
Josephine <maryjohofmeister@gmail.com>
2015-06-29 08:12:11 -0300

    Revert "Confidence Intervals for GLMMS Example Code"
    This reverts commit 723df07485167a2b3ac46df58470db5e25fee0cc.

--- ShinyTutorial.R
+++
@@ -1,9 +0,0 @@
-install.packages("shiny")
-#http://shiny.rstudio.com/tutorial/lesson1/
-library(shiny)
-runExample("01_hello")
-#Set working drive - for work
-setwd("C:/Users/carvalhom/Documents/R")#set data library
-
-runApp("my_app")
-runExample("02_text") # tables and data frames
